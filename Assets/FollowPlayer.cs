68df4ab3c47594c66dd2db32173bd1438dbd64d8
drilonbajrami <54851175+drilonbajrami@users.noreply.github.com>
2019-11-25 13:54:35 +0100

    MiniGame
    MG

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class FollowPlayer : MonoBehaviour
+//{
+//	public Transform PlayerTransfrom;
+//	public Vector3 _cameraOffset;
+//	[Range(0.01f, 1.0f)]
+//	public float SmoothFactor = 0.5f;
+
+//	float rotateOffset;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//		_cameraOffset = transform.position - PlayerTransfrom.position;
+//    }
+
+//	private void LateUpdate()
+//	{
+//		rotateOffset = PlayerTransfrom.rotation.y;
+//		Vector3 newPos = PlayerTransfrom.position + _cameraOffset;
+
+//		//transform.RotateAround(Vector3., rotateOffset);
+//		transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+
+//	}
+//}
