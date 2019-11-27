40e4851330bded1644311115dc74e38ccac5c8b5
Buga Eusebiu Mihail <eusebiu.buga@gmail.com>
2019-11-27 21:23:28 +0100

    Added portfolio app

---
+++ Assets/PosterEvents.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum RoleSelected
+{
+    Artist, Designer, Engineer,GameDetails, Unselected
+}
+
+public class PosterEvents : MonoBehaviour
+{
+    public GameObject RoleSelection, ToggleButton,ChangeRoleButton;
+    
+
+    [HideInInspector]
+    public RoleSelected role;
+
+    void Start()
+    {
+        SetRole(RoleSelected.Unselected);
+
+        RoleSelection.SetActive(false);
+        ChangeRoleButton.SetActive(false);
+    }
+
+    public void SetToGameDetails() { SetRole(RoleSelected.GameDetails); }
+    public void SetToEngineer(){ SetRole(RoleSelected.Engineer); }
+    public void SetToDesigner() { SetRole(RoleSelected.Designer); }
+    public void SetToArtist() { SetRole(RoleSelected.Artist); }
+    public void ResetRole() { SetRole(RoleSelected.Unselected); }
+
+    private void SetRole(RoleSelected _role)
+    {
+        role = _role;
+    }
+
+    public void ShowRoles()
+    {
+        RoleSelection.SetActive(true);
+        ChangeRoleButton.SetActive(false);
+    }
+
+    public void HideRoles()
+    {
+        RoleSelection.SetActive(false);
+        ChangeRoleButton.SetActive(true);
+    }
+
+    public void ShowToggleButton()
+    {
+        ToggleButton.SetActive(true);
+    }
+
+    public void HideToggleButton()
+    {
+        ToggleButton.SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/RoleHandler.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoleHandler : MonoBehaviour
+{
+    [SerializeField]
+    private RoleSelected role;
+
+    [SerializeField]
+    private GameObject handlerObject;
+
+    private int ActiveInformation = 0;
+
+
+    [SerializeField]
+    private GameObject dropDown;
+
+    private PosterEvents posterHandler;
+
+    private bool isSelected = false;
+
+    // Start is called before the first frame update
+
+
+
+    void Start()
+    {
+
+        dropDown.SetActive(false);
+
+        posterHandler = handlerObject.GetComponent<PosterEvents>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (role == RoleSelected.GameDetails)
+        {
+            if (posterHandler.role == role)
+            {
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    transform.GetChild(i).gameObject.SetActive(true);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    transform.GetChild(i).gameObject.SetActive(false);
+                }
+            }
+        }
+        //else if it is anything else other than the game details
+        else
+        {
+            if (posterHandler.role == role)
+            {
+                //code here
+                dropDown.SetActive(true);
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    if (i == ActiveInformation)
+                    {
+                        transform.GetChild(i).gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        transform.GetChild(i).gameObject.SetActive(false);
+                    }
+                }
+
+            }
+            else
+            {
+                ActiveInformation = 0;
+                dropDown.SetActive(false);
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    transform.GetChild(i).gameObject.SetActive(false);
+                }
+            }
+        }
+
+    }
+
+    public void SetInformation(int index)
+    {
+        Debug.Log("information changed to" + index);
+        ActiveInformation = index;
+    }
+
+}
